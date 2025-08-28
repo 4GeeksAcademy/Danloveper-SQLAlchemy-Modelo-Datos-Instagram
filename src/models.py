b4795e4678bf632c5862549f7a62b6a8d53d1f33
Danloveper <danloveper@gmail.com>
2025-08-28 04:01:32 +0000

    Add/primeros modelos de datps

--- src/models.py
+++ src/models.py
@@ -1,19 +1,26 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    __tablename__ = 'user'
 
+    id : Mapped[int] = mapped_column(Integer, primary_key=True)
+    name : Mapped[str] = mapped_column(String(50), nullable=False)
+    email : Mapped[str] = mapped_column(String(40),)
+    password : Mapped[str] = mapped_column(String(60),)
+    
+    posts: Mapped[list['Post']] = relationship(back_populates='user')
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Post(db.Model):
+    __tablename__='post'
+
+    id : Mapped[int] = mapped_column(Integer, primary_key=True)
+    img : Mapped[str] = mapped_column(String)
+    title : Mapped[str] = mapped_column(String)
+    Description : Mapped[str] = mapped_column(String)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user: Mapped['User'] = relationship(back_populates="autor")
